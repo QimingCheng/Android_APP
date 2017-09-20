3bfdb40eb2666d5d34bec040f9922b1f7d0490df
James <54321387@qq.com>
2017-09-20 17:42:58 +0800

    Create CimeFragment.java

---
+++ CriminalIntent/app/src/main/java/com/example/jimcheng/criminalintent/CrimeFragment.java
@@ -0,0 +1,16 @@
+package com.example.jimcheng.criminalintent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by jim.cheng on 2017/9/19.
+ */
+
+public class CrimeFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
