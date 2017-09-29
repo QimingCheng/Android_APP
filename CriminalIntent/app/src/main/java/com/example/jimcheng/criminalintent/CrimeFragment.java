c508a238fef745230f29da07d9e75aa7d326cb58
James <54321387@qq.com>
2017-09-29 09:07:52 +0800

    Add the handle of the button and Textview
    Add the handle of the button and Textview

--- CriminalIntent/app/src/main/java/com/example/jimcheng/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/example/jimcheng/criminalintent/CrimeFragment.java
@@ -9,6 +9,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 /**
@@ -19,6 +21,7 @@ public class CrimeFragment extends Fragment {
     private Crime mCrime;
     private EditText mTitleField;
     private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,9 +49,16 @@ public class CrimeFragment extends Fragment {
             }
         });
 
-        //mDateButton = (Button) v.findViewById(R.id.crime_date);
-
-
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        mDateButton.setText(mCrime.getmDate().toString());
+        mDateButton.setEnabled(false);
+        mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                mCrime.setmSolved(b);
+            }
+        });
         return v;
     }
 
